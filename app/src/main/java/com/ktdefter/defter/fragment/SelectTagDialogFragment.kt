7dcf0b9630ab52344bbb365b888812290181a7fd
flc <mustafaalimutlu@gmail.com>
2021-12-15 00:24:03 +0300

    -code cleanup on some classes. -use Timber instead of Log.

--- app/src/main/java/com/ktdefter/defter/fragment/SelectTagDialogFragment.kt
+++ app/src/main/java/com/ktdefter/defter/fragment/SelectTagDialogFragment.kt
@@ -3,47 +3,35 @@ package com.ktdefter.defter.fragment
 import android.app.AlertDialog
 import android.app.Dialog
 import android.content.Context
-import android.content.DialogInterface
 import android.net.Uri
 import android.os.Bundle
-import android.util.Log
 import android.widget.ArrayAdapter
 import androidx.fragment.app.DialogFragment
-import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import com.ktdefter.defter.R
 import com.ktdefter.defter.data.Bookmark
 import com.ktdefter.defter.data.Tag
 import com.ktdefter.defter.viewmodels.BookmarksViewModel
 import dagger.hilt.android.AndroidEntryPoint
-import kotlinx.android.synthetic.*
-import kotlinx.android.synthetic.main.fragment_select_tag_dialog.*
 import kotlinx.android.synthetic.main.fragment_select_tag_dialog.view.*
+import timber.log.Timber
 
-/**
- * A simple [Fragment] subclass.
- * Activities that contain this fragment must implement the
- * [SelectTagDialogFragment.OnFragmentInteractionListener] interface
- * to handle interaction events.
- * Use the [SelectTagDialogFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 @AndroidEntryPoint
 class SelectTagDialogFragment : DialogFragment() {
     private var listener: OnFragmentInteractionListener? = null
-    private  val bookmarksViewModel: BookmarksViewModel by viewModels<BookmarksViewModel>()
+    private  val bookmarksViewModel: BookmarksViewModel by viewModels()
     lateinit var selectedBookmark: Bookmark
     lateinit var tags: List<Tag>
-    lateinit var allTags: List<Tag>
+    private lateinit var allTags: List<Tag>
 
     override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
         super.onCreateDialog(savedInstanceState)
-        return activity?.let {
+        return activity?.let {activity ->
 
             val selectedTags = allTags.map { tags.contains(it) }
-            Log.d("Defter", "all tags are: ${allTags.map { it.tagName }}")
+            Timber.d("all tags are: " + allTags.map{it.tagName+" "})
             val changes: MutableMap<String, Boolean> = mutableMapOf()
-            val view = it.layoutInflater.inflate(R.layout.fragment_select_tag_dialog, null)
+            val view = activity.layoutInflater.inflate(R.layout.fragment_select_tag_dialog, null)
             view.apply {
                 new_tag_text.setAdapter(
                     ArrayAdapter(this.context,
@@ -53,32 +41,35 @@ class SelectTagDialogFragment : DialogFragment() {
                 new_tag_text.threshold = 1
             }
 
-            AlertDialog.Builder(it)
+            AlertDialog.Builder(activity)
                 .setView(view)
 
                 .setMultiChoiceItems(allTags.map { it.tagName }.toTypedArray(),
-                    allTags.map { tags.contains(it) }.toBooleanArray(),
-                    DialogInterface.OnMultiChoiceClickListener { dialog, which, isChecked ->
-                        changes.put(allTags.map { it.tagName }.get(which), isChecked)
-                    })
+                    allTags.map { tags.contains(it) }.toBooleanArray()
+                ) { _, which, isChecked ->
+                    changes[allTags.map { it.tagName }[which]] = isChecked
+                }
 
                 .setTitle("Select new tag")
 
                 .setNegativeButton("Cancel") { dialog, _ ->
-                        dialog.cancel()
-                    }
+                    dialog.cancel()
+                }
 
-                .setPositiveButton("Add",
-                    DialogInterface.OnClickListener { _, _ ->
-                        changes.mapKeys {
-                            if (it.value == true) {
-                                bookmarksViewModel.addBookmarkTagPair(selectedBookmark.url, it.key)
-                            } else {
-                            bookmarksViewModel.deleteBookmarkTagPair(selectedBookmark.url, it.key)
-                            }
+                .setPositiveButton("Add"
+                ) { _, _ ->
+                    changes.mapKeys {
+                        if (it.value) {
+                            bookmarksViewModel.addBookmarkTagPair(selectedBookmark.url, it.key)
+                        } else {
+                            bookmarksViewModel.deleteBookmarkTagPair(
+                                selectedBookmark.url,
+                                it.key
+                            )
                         }
-                        onPositiveClick(view.new_tag_text.text.toString())
-                    })
+                    }
+                    onPositiveClick(view.new_tag_text.text.toString())
+                }
                 .create()
         } ?: throw IllegalStateException("Main Activity cannot be null")
     }
@@ -101,7 +92,7 @@ class SelectTagDialogFragment : DialogFragment() {
         if (context is OnFragmentInteractionListener) {
             listener = context
         } else {
-            throw RuntimeException(context.toString() + " must implement OnFragmentInteractionListener")
+            throw RuntimeException("$context must implement OnFragmentInteractionListener")
         }
     }
 
@@ -110,37 +101,8 @@ class SelectTagDialogFragment : DialogFragment() {
         listener = null
     }
 
-    /**
-     * This interface must be implemented by activities that contain this
-     * fragment to allow an interaction in this fragment to be communicated
-     * to the activity and potentially other fragments contained in that
-     * activity.
-     *
-     *
-     * See the Android Training lesson [Communicating with Other Fragments]
-     * (http://developer.android.com/training/basics/fragments/communicating.html)
-     * for more information.
-     */
     interface OnFragmentInteractionListener {
         // TODO: Update argument type and name
         fun onFragmentInteraction(uri: Uri)
-    }
-
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment SelectTagDialogFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance() =
-            SelectTagDialogFragment().apply {
-                arguments = Bundle().apply {
-                }
-            }
     }
 }
